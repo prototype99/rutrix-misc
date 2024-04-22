53cdff1b1819fcd3ae1f554653f9c4bed5dcb680
whatif12 <whatif1208@yahoo.com>
2024-04-22 13:03:37 -0500

    Refactors

--- Source/RV2R_RutsStuff/Patches/Patch_CanGiveCommand.cs
+++ Source/RV2R_RutsStuff/Patches/Patch_CanGiveCommand.cs
@@ -12,30 +12,28 @@ namespace RV2R_RutsStuff
         [HarmonyPostfix]
         public static void TamedCommands(ref bool __result, Pawn pawn)
         {
-            if (!pawn.Dead && !pawn.InMentalState && !pawn.Downed && !pawn.IsBurning())
+            if(pawn.Dead) return;
+            if(pawn.InMentalState) return;
+            if(pawn.Downed) return;
+            if (pawn.IsBurning()) return;
+
+            if (pawn.IsColonistPlayerControlled)
             {
-                if (pawn.IsColonistPlayerControlled)
-                {
-                    __result = true;
-                    return;
-                }
-                if (pawn.IsAnimal() && pawn.IsActivePredator())
-                {
-                    if (RV2R_Utilities.GetHighestPreySkillLevel(pawn, SkillDefOf.Animals) >= pawn.GetStatValue(StatDefOf.MinimumHandlingSkill) * 2)
-                    {
-                        __result = true;
-                        return;
-                    }
-                    if (pawn.training != null)
-                    {
-                        Pawn_PlayerSettings playerSettings = pawn.playerSettings;
-                        if (playerSettings != null)
-                            if (playerSettings.RespectedMaster != null && (pawn.playerSettings.RespectedMaster.Position.DistanceTo(pawn.Position) <= 8f || pawn.playerSettings.RespectedMaster.IsPreyOf(pawn)))
-                                __result = true;
-                    }
-                }
+                __result = true;
+                return;
             }
-            return;
+            if(!pawn.IsAnimal()) return;
+            if (!pawn.IsActivePredator()) return;
+            if(RV2R_Utilities.GetHighestPreySkillLevel(pawn, SkillDefOf.Animals) >= pawn.GetStatValue(StatDefOf.MinimumHandlingSkill) * 2)
+            {
+                __result = true;
+                return;
+            }
+            if(pawn.training == null) return;
+            Pawn_PlayerSettings _PlayerSettings = pawn.playerSettings;
+            if (_PlayerSettings == null) return;
+            if(_PlayerSettings.RespectedMaster == null) return;
+            __result = (pawn.playerSettings.RespectedMaster.Position.DistanceTo(pawn.Position) <= 8f || pawn.playerSettings.RespectedMaster.IsPreyOf(pawn));
         }
     }
 }
