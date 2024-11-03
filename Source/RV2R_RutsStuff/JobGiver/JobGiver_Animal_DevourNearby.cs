1d04b885b41514b8b30b927a2fdb9f1ae5afa816
whatif12 <whatif1208@yahoo.com>
2024-11-03 11:55:28 -0600

    Refactored Job Giver DevourNearby

--- Source/RV2R_RutsStuff/JobGiver/JobGiver_Animal_DevourNearby.cs
+++ Source/RV2R_RutsStuff/JobGiver/JobGiver_Animal_DevourNearby.cs
@@ -19,75 +19,122 @@ namespace RV2R_RutsStuff
             return jobGiver_Animal_DevourNearby;
         }
 
+        private Pawn ActivePawn;
+        private Pawn Prey;
+        private VorePathDef VorePathDef;
+
         protected override Job TryGiveJob(Pawn pawn)
         {
-            if (!pawn.CanParticipateInVore(out string _))
-                return null;
-
-            if (GenAI.InDangerousCombat(pawn))
-                return null;
-
-            if (GenAI.EnemyIsNear(pawn, radius))
-                return null;
-
             try
             {
-                bool predicate(Thing t)
-                {
-                    Pawn target = (Pawn)t;
-                    return (target.Downed || !pawn.Faction.HostileTo(target.Faction))
-                        && pawn.CanVore(target, out _)
-                        && pawn.CanReserve(target, 1, -1, null, false)
-                        && !target.IsMechanoid()
-                        && !target.IsForbidden(pawn)
-                        && target.Map.designationManager.DesignationOn(target, RV2R_Common.Devour) != null;
-                }
-                Pawn prey = (Pawn)GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Pawn), PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false, false, false), this.radius, predicate, null, 0, -1, false, RegionType.Set_Passable, false);
-                if (prey == null)
-                    return null;
-
-                VoreJob voreJob = VoreJobMaker.MakeJob(VoreJobDefOf.RV2_VoreInitAsPredator, prey);
-                voreJob.targetA = prey;
-                voreJob.Initiator = pawn;
-                voreJob.IsForced = true;
-                voreJob.count = 1;
-
-                VorePathDef vorePathDef;
-
-                List<VoreGoalDef> list = DefDatabase<VoreGoalDef>.AllDefsListForReading.Where((VoreGoalDef goal) => goal.IsLethal).ToList();
-
-                IEnumerable<VorePathDef> interaction = VoreInteractionManager.Retrieve(new VoreInteractionRequest(pawn, prey, VoreRole.Predator, true, false, false, null, null, null, null, list)).ValidPaths;
-
-                if (!interaction.EnumerableNullOrEmpty<VorePathDef>())
-                {
-                    vorePathDef = interaction.RandomElement<VorePathDef>();
-                    RV2Log.Message(pawn.LabelShort + " eating hostile " + prey.LabelShort + " via " + vorePathDef.ToString(), "Jobs");
-                    voreJob.VorePath = vorePathDef;
-                    voreJob.IsForced = true;
-                    return voreJob;
-                }
-                RV2Log.Message("Predator " + pawn.LabelShort + " can't fatal vore " + prey.LabelShort + "; checking for healing instead", true, "Jobs");
-
-                list = new List<VoreGoalDef> { VoreGoalDefOf.Heal };
-                if (prey.Faction.IsPlayer && !prey.health.HasHediffsNeedingTend())
-                    list = DefDatabase<VoreGoalDef>.AllDefsListForReading.Where((VoreGoalDef goal) => !goal.IsLethal).ToList();
-                interaction = VoreInteractionManager.Retrieve(new VoreInteractionRequest(pawn, prey, VoreRole.Predator, true, false, false, null, null, null, null, list)).ValidPaths;
-                if (interaction.EnumerableNullOrEmpty<VorePathDef>())
-                {
-                    RV2Log.Message("Predator " + pawn.LabelShort + " can't heal vore " + prey.LabelShort + ", no predation", true, "Jobs");
-                    return null;
-                }
-                vorePathDef = interaction.RandomElement<VorePathDef>();
-                RV2Log.Message(pawn.LabelShort + " capturing hostile " + prey.LabelShort + " via " + vorePathDef.ToString(), "Jobs");
-                voreJob.VorePath = vorePathDef;
-                voreJob.IsForced = true;
-                return voreJob;
+                ActivePawn = pawn;
+                return _TryGiveJob();
+                
             }
             catch (Exception e)
             {
                 Log.Warning("RV-2R: Something went wrong when " + pawn.LabelShort + " tried to nom downed enemy: " + e);
                 return null;
+            } finally { 
+                ActivePawn = null;
+                Prey = null;
+                VorePathDef = null;
             }
+        }
+
+        private Job _TryGiveJob()
+        {
+            if (GenAI.InDangerousCombat(ActivePawn))
+                return null;
+
+            if (GenAI.EnemyIsNear(ActivePawn, radius))
+                return null;
+
+            if (!ActivePawn.CanParticipateInVore(out string _))
+                return null;
+
+            PickPrey();
+            if (Prey == null) return null;
+
+            PickPath();
+            if (VorePathDef == null) return null;
+         
+            return MakeJob();
+        }
+
+        private void PickPath()
+        {
+            PickPath(ValidLethalGoals());
+            if (VorePathDef != null) return;
+
+            RV2Log.Message($"Predator {ActivePawn.LabelShort} can't lethal vore {Prey}; checking for healing instead", "Jobs");
+            if (MayHeal())
+                PickPath(ValidHealingGoals());
+            if(VorePathDef != null) return;
+
+            PickPath(ValidNonLethalGoals());
+        }
+        public void PickPath(IEnumerable<VoreGoalDef> ValidGoals)
+        {
+            var request = new VoreInteractionRequest(ActivePawn, Prey, VoreRole.Predator, true, goalWhitelist: ValidGoals.ToList());
+            IEnumerable<VorePathDef> interaction = VoreInteractionManager.Retrieve(request).ValidPaths;
+            VorePathDef = interaction.FirstOrDefault();
+        }
+        private bool MayHeal()
+        {
+            if (!Prey.Faction.IsPlayer) return false;
+            if (!Prey.health.HasHediffsNeedingTend()) return false;
+
+            return true;
+        }
+        public IEnumerable<VoreGoalDef> ValidHealingGoals()
+        {
+            yield return VoreGoalDefOf.Heal;
+        }
+        public Job MakeJob()
+        {
+            if (VorePathDef == null) return null;
+
+            VoreJob voreJob = VoreJobMaker.MakeJob(VoreJobDefOf.RV2_VoreInitAsPredator, ActivePawn);
+            voreJob.targetA = Prey;
+            voreJob.IsForced = true;
+            voreJob.count = 1;
+            voreJob.IsForced = true;
+            voreJob.VorePath = VorePathDef;
+
+            RV2Log.Message($"{ActivePawn.LabelShort} eating hostile {Prey.LabelShort} via {VorePathDef}", "Jobs");
+
+            return voreJob;
+        }
+        public IEnumerable<VoreGoalDef> ValidLethalGoals()
+        {
+            return DefDatabase<VoreGoalDef>.AllDefsListForReading.Where((VoreGoalDef goal) => goal.IsLethal);
+        }
+        public IEnumerable<VoreGoalDef> ValidNonLethalGoals()
+        {
+            return DefDatabase<VoreGoalDef>.AllDefsListForReading.Where((VoreGoalDef goal) => !goal.IsLethal);
+        }
+        public void PickPrey()
+        {
+            Prey = GenClosest.ClosestThingReachable(ActivePawn.Position, ActivePawn.Map
+                , ThingRequest.ForGroup(ThingRequestGroup.Pawn)
+                , PathEndMode.OnCell
+                , TraverseParms.For(ActivePawn)
+                , this.radius
+                , IsValidTarget
+                ) as Pawn;
+        }
+        public bool IsValidTarget(Thing t)
+        {
+            if (!(t is Pawn target)) return false;
+
+            if (!target.Downed && !ActivePawn.Faction.HostileTo(target.Faction)) return false;
+            if (target.IsMechanoid()) return false;
+            if (target.IsForbidden(ActivePawn)) return false;
+            if (target.Map.designationManager.DesignationOn(target, RV2R_Common.Devour) == null) return false;
+            if (!ActivePawn.CanReserve(target)) return false;
+            if (!ActivePawn.CanVore(target, out _)) return false;
+            return true;
         }
     }
 }
