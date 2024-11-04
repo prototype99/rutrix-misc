bcbd741d98566fe7c9a38a8940789a1ac6d922d2
whatif12 <whatif1208@yahoo.com>
2024-11-03 18:55:11 -0600

    REfactored Job Giver Animal Vore Nearby

--- Source/RV2R_RutsStuff/JobGiver/JobGiver_Animal_VoreNearby.cs
+++ Source/RV2R_RutsStuff/JobGiver/JobGiver_Animal_VoreNearby.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Verse;
 using Verse.AI;
+using static RimWorld.PsychicRitualRoleDef;
 using static RV2R_RutsStuff.Patch_RV2R_Settings;
 
 namespace RV2R_RutsStuff
@@ -19,78 +20,139 @@ namespace RV2R_RutsStuff
             return jobGiver_Animal_EndoNearby;
         }
 
+        private Pawn ActivePawn;
+        private Pawn Prey;
+        private VorePathDef VorePathDef;
+
         protected override Job TryGiveJob(Pawn pawn)
         {
-            if (!pawn.CanParticipateInVore(out string reason))
-                return null;
-
-            if (GenAI.InDangerousCombat(pawn))
-                return null;
-
             try
             {
-                Pawn prey = null;
-
-                if (RV2_Rut_Settings.rutsStuff.VornyBonds && pawn.relations.GetDirectRelationsCount(PawnRelationDefOf.Bond) > 0)
-                {
-                    Pawn bond = pawn.relations.GetFirstDirectRelationPawn(PawnRelationDefOf.Bond);
-                    if (bond != null
-                     && !GenAI.EnemyIsNear(pawn, radius)
-                     && !RV2R_Utilities.IsBusy(pawn, bond)
-                     && pawn.Position.DistanceTo(bond.Position) < radius
-                     && pawn.CanReserve(bond, 1, -1, null, false)
-                     && pawn.CanVore(bond, out reason)
-                     && Rand.Chance(0.20f))
-                        prey = bond;
-                }
-                if (prey == null)
-                {
-                    bool predicate(Thing t)
-                    {
-                        Pawn pawn3 = (Pawn)t;
-                        return pawn3 != pawn
-                            && (RV2_Rut_Settings.rutsStuff.PlayVoreIndescriminate || (pawn3.Faction != null && pawn3.Faction.IsPlayer))
-                            && !GenAI.InDangerousCombat(pawn3)
-                            && pawn.CanReserve(pawn3, 1, -1, null, false)
-                            && !pawn3.IsForbidden(pawn)
-                            && !GenAI.EnemyIsNear(pawn, radius)
-                            && !RV2R_Utilities.IsBusy(pawn, pawn3)
-                            && !RV2R_Utilities.IsFreeHostileToColony(pawn, pawn3)
-                            && pawn.CanVore(pawn3, out reason)
-                            && (!RV2R_Utilities.IsSapient(pawn3) || Rand.Chance(RV2_Rut_Settings.rutsStuff.PlayVoreColonistBias));
-                    }
-                    prey = (Pawn)GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Pawn), PathEndMode.OnCell, TraverseParms.For(pawn, Danger.Some, TraverseMode.ByPawn, false, false, false), this.radius, predicate, null, 0, -1, false, RegionType.Set_Passable, false);
-                }
-                if (prey == null)
-                    return null;
-                List<VoreGoalDef> list = DefDatabase<VoreGoalDef>.AllDefsListForReading.Where((VoreGoalDef goal) => !goal.IsLethal && goal.defName != "Store").ToList();
-                if (!pawn.Name.Numerical)
-                    list.Append(VoreGoalDefOf.Store);
-
-                if (RV2_Rut_Settings.rutsStuff.FatalPlayVore)
-                    list = DefDatabase<VoreGoalDef>.AllDefsListForReading;
-                VoreInteraction voreInteraction = VoreInteractionManager.Retrieve(new VoreInteractionRequest(pawn, prey, VoreRole.Predator, true, false, false, null, null, null, null, list));
-                if (voreInteraction.ValidPaths.EnumerableNullOrEmpty<VorePathDef>())
-                {
-                    RV2Log.Message("Predator " + pawn.LabelShort + " can't play-pred their target " + prey.LabelShort, "Jobs");
-                    return null;
-                }
-                VorePathDef vorePathDef = (voreInteraction.PreferredPath ?? voreInteraction.ValidPaths.RandomElement<VorePathDef>());
-                RV2Log.Message(pawn.LabelShort + " play-predding " + prey.LabelShort + " via " + vorePathDef.label, "Jobs");
-                VoreProposal_TwoWay voreProposal_TwoWay = new VoreProposal_TwoWay(voreInteraction.Predator, voreInteraction.Prey, pawn, prey, vorePathDef);
-                VoreJob voreJob = VoreJobMaker.MakeJob(prey.IsColonistPlayerControlled ? VoreJobDefOf.RV2_ProposeVore : VoreJobDefOf.RV2_VoreInitAsPredator, pawn);
-                voreJob.targetA = prey;
-                voreJob.Proposal = voreProposal_TwoWay;
-                voreJob.VorePath = vorePathDef;
-                voreJob.Initiator = pawn;
-                voreJob.count = 1;
-                return voreJob;
+                ActivePawn = pawn;
+                return _TryGiveJob();
             }
             catch (Exception e)
             {
                 Log.Warning("RV-2R: Something went wrong when " + pawn.LabelShort + " tried to play-pred: " + e);
                 return null;
             }
+            finally
+            {
+                ActivePawn = null;
+                Prey = null;
+                VorePathDef = null;
+            }
+        }
+
+        private protected Job _TryGiveJob()
+        {
+            if (GenAI.InDangerousCombat(ActivePawn)) return null;
+            if (GenAI.EnemyIsNear(ActivePawn, radius)) return null;
+            if (!ActivePawn.CanParticipateInVore(out string reason)) return null;
+
+
+            PickPrey();
+            if (Prey == null) return null;
+            PickVorePathDef();
+            if(VorePathDef == null) return null;
+            RV2Log.Message($"{ActivePawn.Label} play-predding {Prey.Label} via {VorePathDef.label}", "Jobs");
+
+            return MakeJob();
+        }
+
+        public Job MakeJob()
+        {
+            VoreProposal_TwoWay voreProposal_TwoWay = new VoreProposal_TwoWay(ActivePawn, Prey, ActivePawn, Prey, VorePathDef);
+            var initDef = Prey.IsColonistPlayerControlled ? VoreJobDefOf.RV2_ProposeVore : VoreJobDefOf.RV2_VoreInitAsPredator;
+            VoreJob Job = VoreJobMaker.MakeJob(initDef, ActivePawn);
+            Job.targetA = Prey;
+            Job.Proposal = voreProposal_TwoWay;
+            Job.VorePath = VorePathDef;
+            Job.count = 1;
+            return Job;
+        }
+        public void PickVorePathDef()
+        {
+            var request = new VoreInteractionRequest(ActivePawn, Prey, VoreRole.Predator, true, goalWhitelist: ValidGoals().ToList());
+            var Interaction = new VoreInteraction(request);
+            VorePathDef = Interaction.PreferredPath ?? Interaction.ValidPaths.RandomElement();
+        }
+        public IEnumerable<VoreGoalDef> ValidGoals()
+        {
+            foreach(var def in DefDatabase<VoreGoalDef>.AllDefsListForReading)
+            {
+                if (def.IsLethal && !RV2_Rut_Settings.rutsStuff.FatalPlayVore) continue;
+                yield return def;
+            }
+        }
+        private void PickPrey()
+        {
+            PickVornyBond();
+            if (Prey == null) return;
+            PickIndescriminate();
+        }
+
+        private void PickIndescriminate()
+        {
+            Prey = GenClosest.ClosestThingReachable(
+                ActivePawn.Position,
+                ActivePawn.Map,
+                ThingRequest.ForGroup(ThingRequestGroup.Pawn),
+                PathEndMode.OnCell,
+                TraverseParms.For(ActivePawn),
+                this.radius,
+                IsValidForIndescriminate
+            ) as Pawn;
+        }
+        private bool IsValidForIndescriminate(Thing t)
+        {
+            if(!(t is Pawn prey)) return false;
+            if(prey.Faction?.IsPlayer == true)
+            {
+                if (!RV2_Rut_Settings.rutsStuff.PlayVoreIndescriminate) 
+                    return false;
+            }
+
+            if (GenAI.InDangerousCombat(prey)) return false;
+            if (ActivePawn.CanReserve(prey)) return false;
+            if (prey.IsForbidden(ActivePawn)) return false;
+
+            if (RV2R_Utilities.IsBusy(ActivePawn, prey)) return false;
+            if (RV2R_Utilities.IsFreeHostileToColony(ActivePawn, prey)) return false;
+            if (!ActivePawn.CanVore(prey, out _)) return false;
+
+            if (!RV2R_Utilities.IsSapient(prey)) return true;
+            if (Rand.Chance(RV2_Rut_Settings.rutsStuff.PlayVoreColonistBias)) return true;
+
+            return false;
+        }
+
+        private void PickVornyBond()
+        {
+            if (!RV2_Rut_Settings.rutsStuff.VornyBonds) return;
+            if (!Rand.Chance(.2f)) return;
+            var validBonds = GetValidBonds();
+            if(validBonds.EnumerableNullOrEmpty()) return;
+            Prey = validBonds.RandomElement();
+        }
+        public IEnumerable<Pawn> GetValidBonds()
+        {
+            return ActivePawn.relations.DirectRelations.Where(r => BondRelationsDefs().Contains(r.def) && IsValidForVornyBond(r)).Select(r=>r.otherPawn);
+        }
+
+        private bool IsValidForVornyBond(DirectPawnRelation relation)
+        {
+            var prey = relation.otherPawn;
+            if (ActivePawn.CanReserve(prey)) return false;
+            if (prey.Position.DistanceTo(ActivePawn.Position) > radius) return false;
+            if (ActivePawn.CanVore(prey, out _)) return false;
+
+            return true;
+        }
+
+        public IEnumerable<PawnRelationDef> BondRelationsDefs()
+        {
+            yield return PawnRelationDefOf.Bond;
         }
     }
 }
